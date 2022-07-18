df29e27e9a3c65dfa3f8b70c1247887e8f99f9bc
ashleylinda90 <ashley.fillmore@keyin.com>
2022-07-18 09:41:43 -0230

    Commit

---
+++ src/main/java/com/keyin/rest/domain/Member.java
@@ -0,0 +1,79 @@
+package com.keyin.rest.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private Date membershipStartDate;
+    private double membershipDuration;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getMembershipStartDate() {
+        return membershipStartDate;
+    }
+
+    public void setMembershipStartDate(Date membershipStartDate) {
+        this.membershipStartDate = membershipStartDate;
+    }
+
+    public double getMembershipDuration() {
+        return membershipDuration;
+    }
+
+    public void setMembershipDuration(double membershipDuration) {
+        this.membershipDuration = membershipDuration;
+    }
+}
